f9b0bcdac41c350040cd5596dbb32ca1db2f71fa
Sofie <29145005+sofie29@users.noreply.github.com>
2022-03-25 08:30:31 +0100

    implemented tryFitOverlapping method

--- Optimierungsalgorithmen/src/header/Algorithm/BoundingBox.h
+++ Optimierungsalgorithmen/src/header/Algorithm/BoundingBox.h
@@ -20,6 +20,10 @@ public:
 
 	bool tryFit(int rect_width, int rect_height, int& x_out, int& y_out); // dont forget to call addRectangleIndex / removeRectangleIndex after calling tryFit
 	bool tryFit(class RectangleHolder* rectHolder, int boundingBoxIndex);
+
+	// box_width, box_x and box_y refer to the top level bounding box, indices must not contain index of the rectangle which is placed, temperature t
+	bool tryFitOverlapping(class RectangleHolder* rectHolder, int boundingBoxIndex, int t, std::vector<RectangleHolder*>* const rectangles, std::vector<int> indices, int box_width, int box_x, int box_y);
+	
 	void removeLowerLevelBoundingBoxes();
 	void addRectangleIndex(int index);
 	void removeRectangleIndex(int index);
--- Optimierungsalgorithmen/src/src/Algorithm/BoundingBox.cpp
+++ Optimierungsalgorithmen/src/src/Algorithm/BoundingBox.cpp
@@ -45,7 +45,7 @@ BoundingBox::BoundingBox(int rect_width, int rect_height, int x, int y, std::vec
 	rect_width(rect_width),
 	x(x),
 	y(y),
-	first(nullptr), // TODO ???
+	first(nullptr),
 	second(nullptr),
 	rectangleIndices(indices),
 	numberOfRectangles(indices.size())
@@ -175,6 +175,76 @@ bool BoundingBox::tryFit(RectangleHolder* rectHolder, int boundingBoxIndex)
 	return false;
 }
 
+bool BoundingBox::tryFitOverlapping(RectangleHolder* rectHolder, int boundingBoxIndex, int t, std::vector<RectangleHolder*>* const rectangles, std::vector<int> indices, int box_width, int box_x, int box_y)
+{
+	QRectF& rect = rectHolder->getRectRef();
+	int rect_width = rect.width();
+	int rect_height = rect.height();
+	if (this->first || this->second) { // BoundingBox is not empty
+
+	// try to place rectangle at first BoundingBox
+		if (this->first && this->first->tryFitOverlapping(rectHolder, boundingBoxIndex, t, rectangles, indices, box_width, box_x, box_y));
+			return true;
+
+		// try to place rectangle at second BoundingBox
+		if (this->second && this->second->tryFitOverlapping(rectHolder, boundingBoxIndex, t, rectangles, indices, box_width, box_x, box_y))
+			return true;
+
+		return false; // rectangle cannot be placed in neither the first nor the second BoundingBox
+	}
+
+	else { // BoundingBox is empty
+
+		// in case there is no overlapping, call the simple tryFit method
+		if (rect_width <= this->rect_width && rect_height <= this->rect_height) {
+			return this->tryFit(rectHolder, boundingBoxIndex); // need this method because of correct child box creation
+		}
+
+		// box_width, box_x and box_y refer to the top level bounding box
+		int overlapTopLevelBoxX = this->x - box_x + rect_width - box_width;
+		int overlapTopLevelBoxY = this->y - box_y + rect_height - box_width;
+
+		// shift rectangle left or top until it is totally inside the global bounding box
+		int rectPosX = overlapTopLevelBoxX > 0 ? this->x - overlapTopLevelBoxX : this->x;
+		int	rectPosY = overlapTopLevelBoxY > 0 ? this->y - overlapTopLevelBoxY : this->y;
+
+
+		// ATTENTION: indices must not contain index of the rectangle which should be placed
+		for (int i : indices) {
+			QRectF& rect2 = (*rectangles)[i]->getRectRef();
+
+			int x_overlap = std::min(rectPosX + rect_width, (int)(rect2.x() + rect2.width())) - std::max(rectPosX, (int)rect2.x());
+			int y_overlap = std::min(rectPosY + rect_height, (int)(rect2.y() + rect2.height())) - std::max(rectPosY, (int)rect2.y());
+			int area_overlap = (x_overlap > 0 && y_overlap > 0) ? x_overlap * y_overlap : 0;
+			
+			int area_1 = rect.width() * rect.height();
+			int area_2 = rect2.width() * rect2.height();
+			int area_max = std::max(area_1, area_2);
+
+			int overlap = area_overlap / area_max;
+
+			if (overlap > t) {
+				return false;
+			}
+		}
+
+		// all overlappings are smaller than parameter t
+
+		rect.moveTopLeft(QPointF(rectPosX, rectPosY));
+		rectHolder->setBoundingBoxIndex(boundingBoxIndex);
+
+		this->first = rect_width > this->rect_width ?
+			std::make_shared<BoundingBox>(0, 0, this->x, this->y) : // position can be chosen arbitrarily
+			std::make_shared<BoundingBox>(this->rect_width - rect_width, this->rect_height, this->x + rect_width, this->y);
+
+		this->second = rect_height > this->rect_height ?
+			std::make_shared<BoundingBox>(0, 0, this->x, this->y) : // position can be chosen arbitrarily
+			std::make_shared<BoundingBox>(this->rect_width, this->rect_height - rect_height, this->x, this->y + rect_height);
+
+		return true;
+	}
+}
+
 int BoundingBox::getXPos() const
 {
 	return x;
@@ -221,6 +291,4 @@ void BoundingBox::removeRectangleIndex(int index) {
 	--numberOfRectangles;
 	
 	return;
-
-	// TODO: if (--numberOfRectangles == 0) delete this bounding box
 }
